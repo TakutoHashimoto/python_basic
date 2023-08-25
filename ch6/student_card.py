0cca53f10b763d89048508ea338ba3644e41bef3
TakutoHashimoto <tkthsmt0531@gmail.com>
2023-08-26 02:09:54 +0900

    update

--- ch6/student_card.py
+++ ch6/student_card.py
@@ -1,7 +1,14 @@
 class StudentCard:
     def __init__(self):
         print("初期化メソッド内の処理です")
+        self.id = 0
+        self.name = "未定"
 
 
 a = StudentCard()
 b = StudentCard()
+
+a.id, a.name = 1234, "鈴木太郎"
+b.id, b.name = 1235, "佐藤花子"
+print(f"a.id: {a.id}, a.name: {a.name}")
+print(f"b.id: {b.id}, b.name: {b.name}")
