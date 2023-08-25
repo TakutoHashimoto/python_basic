b5121d72b2a69a3c9aec43dd47d003bf8f4d5b0c
TakutoHashimoto <tkthsmt0531@gmail.com>
2023-08-26 02:23:33 +0900

    update

--- ch6/student_card.py
+++ ch6/student_card.py
@@ -1,17 +1,15 @@
 class StudentCard:
-    # クラス変数
-    school_name = "Python大学"
-
     def __init__(self, id, name):
-        print("初期化メソッド内の処理です")
         self.id = id
         self.name = name
+    
+    def print_info(self):
+        print(f"学籍番号: {self.id}")
+        print(f"氏名: {self.name}")
 
 
 a = StudentCard(1234, "鈴木太郎")
 b = StudentCard(1235, "佐藤花子")
 
-print(f"a.id: {a.id}, a.name: {a.name}")
-print(f"b.id: {b.id}, b.name: {b.name}")
-
-print(StudentCard.school_name)
+a.print_info()
+b.print_info()
